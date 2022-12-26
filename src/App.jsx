991ecf968929df8c745494ca54e3f93b9ea423d1
Fabian Gomez <fabian37gomez@gmail.com>
2022-12-26 10:38:03 -0500

    feat: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import { Header } from './components/header/Header';
 import { Home } from './pages/home/Home';
+import './App.css';
 
 const App = () => (
-	<>
+	<BrowserRouter>
 		<Header />
 		<Home />
-	</>
+	</BrowserRouter>
 );
 
 export default App;
--- src/components/header/headerNav/HeaderNav.jsx
+++ src/components/header/headerNav/HeaderNav.jsx
@@ -1,3 +1,21 @@
+import { Link } from 'react-router-dom';
+import styles from './HeaderNav.module.css';
+
 export const HeaderNav = () => {
-	return <nav>Barra de navegation</nav>;
+	return (
+		<nav className={styles.navContainer}>
+			<Link className={styles.navItem} to='/'>
+				HOME
+			</Link>
+			<Link className={styles.navItem} to='/'>
+				ABOUT
+			</Link>
+			<Link className={styles.navItem} to='/'>
+				CONTACT
+			</Link>
+			<Link className={styles.navItem} to='/'>
+				WRITE
+			</Link>
+		</nav>
+	);
 };
