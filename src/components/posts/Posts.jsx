52846ff56245075a8f0fe1e04ec449f0fa0c99b9
Fabian Gomez <fabian37gomez@gmail.com>
2022-12-27 01:17:00 -0500

    feat: add post and posts components

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,1 +1,18 @@
-export const Posts = () => <div>Posts</div>;
+import posts from '../../constants/posts.json';
+import { Post } from '../post/Post';
+import styles from './Posts.module.css';
+
+export const Posts = () => (
+	<div className={styles.posts}>
+		{posts.map((post, index) => (
+			<Post
+				key={index}
+				category={post.category}
+				image={post.image}
+				overview={post.overview}
+				time={post.time}
+				title={post.title}
+			/>
+		))}
+	</div>
+);
