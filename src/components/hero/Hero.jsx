8a0301e3a70feea74ff85650660bd54165d45d4b
Fabian Gomez <fabian37gomez@gmail.com>
2022-12-26 20:45:56 -0500

    feat: h1 tag was modified and css styles

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,10 +4,10 @@ import image from '../../assets/foto6.webp';
 export const Hero = () => {
 	return (
 		<section className={styles.heroContainer}>
-			<h1 className={styles.titleContainer}>
-				<span className={styles.firstTitle}>React & Node</span>
-				<span className={styles.secondTitle}>BLOG</span>
-			</h1>
+			<div className={styles.titleContainer}>
+				<h1 className={styles.firstTitle}>React & Node</h1>
+				<h2 className={styles.secondTitle}>BLOG</h2>
+			</div>
 			<img className={styles.img} src={image} alt='flowers and sunset' />
 		</section>
 	);
